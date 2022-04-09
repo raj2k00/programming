4e6aa606b38db0126efedbece190560a52b1dae0
MOHAN RAJ <mohanraj.2k00@gmail.com>
2022-04-09 13:23:28 +0530

    added comments

--- guessingGame.cpp
+++ guessingGame.cpp
@@ -3,18 +3,25 @@ using namespace std;
 
 int main()
 {
-    int secretNum = 7;
-    int guess;
+    int secretNum = 7; // Secret Number
+    int guess;         // Max Guess Count
     int guessCount = 3;
-    cout << "Guess the number to win (1 - 10)" << endl
+
+    cout << "Guess the number to win (1 - 10)" << endl // Helper text about the game
          << "Only 3 chances" << endl;
+
+    /*  Breaks the loop when the guess is correct or guess limit reached
+      Do While loop decrements guessCount value for wrong guesses */
+
     do
     {
         cout << "Enter the number" << endl;
         cin >> guess;
+
         guessCount--;
     } while (secretNum != guess && guessCount != 0);
 
+    // Prints the OUTPUT when the loop breaks
     if (guessCount)
     {
         cout << "You win the Guessing game" << endl;
