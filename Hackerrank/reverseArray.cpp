e90abe387b5fb8b54818a4076b6498ea35d2e8cc
MOHAN RAJ <mohanraj.2k00@gmail.com>
2022-04-15 20:37:40 +0530

    HackerRank Problems

---
+++ Hackerrank/reverseArray.cpp
@@ -0,0 +1,27 @@
+/*
+  Problem statement -  https://www.hackerrank.com/challenges/arrays-introduction/problem
+  paste the code in the code editor to see the results
+*/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n, i = 0;
+    cin >> n;
+
+    int *A = new int[n]; // Dynamic Allocation of Array memory
+
+    while (cin >> A[i++]) // Runs the loop while all the inputs are stored
+        ;
+
+    // prints the array form reverse while the n is true (n is a size of array)
+    // !IMPORTANT we are decrementing the n value so once it reached 0 loop ends
+    // " && n " checks whether n is true or false if fasle loop terminates
+    while (cout << A[--n] << ' ' && n)
+        ;
+
+    delete[] A; // delete the array to avoid memory leak
+
+    return 0;
+}
