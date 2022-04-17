6efa988494516141345be9f7852c685c3e325bf1
MOHAN RAJ <mohanraj.2k00@gmail.com>
2022-04-17 12:03:45 +0530

    string stream

---
+++ Basics/stringStream.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string str = "The great man bobby lashley";
+    // string str;
+    // cin >> str;
+    stringstream s(str); // Breaking inputs into words using stringstream
+
+    // creating tempory container for storing individual words
+    string word;
+    int count = 0;
+    // we don't know how many are there but do know whats the exit case then use while loop
+    // getting the each word from the stream to word variable
+    while (s >> word)
+    {
+        cout << word << endl;
+        ++count;
+    }
+    cout << "Total number of words " << count;
+    return 0;
+}
